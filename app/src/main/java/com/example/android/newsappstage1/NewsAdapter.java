66db36d18bda468a2b88d1eb5a5da23c71f7fe0c
Innoxe <innodima@gmail.com>
2018-06-03 20:12:51 +0200

    Finish adapter (maybe)

--- app/src/main/java/com/example/android/newsappstage1/NewsAdapter.java
+++ app/src/main/java/com/example/android/newsappstage1/NewsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.newsappstage1;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -10,6 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -54,9 +61,9 @@ public class NewsAdapter extends ArrayAdapter<News> {
         titleView.setText(currentNews.getHeadline());
 
         // Find the TextView with view ID lead
-        TextView titleView = listItemView.findViewById(R.id.trailText);
+        TextView trailView = listItemView.findViewById(R.id.trailText);
         // Display the healine of the current news in that TextView
-        titleView.setText(currentNews.getHeadline());
+        trailView.setText(currentNews.getHeadline());
 
         // Find the TextView with view ID section_name
         TextView sectionNameView = listItemView.findViewById(R.id.section_name);
@@ -79,10 +86,11 @@ public class NewsAdapter extends ArrayAdapter<News> {
         // Find the ImageView with view ID image
         ImageView imageView = listItemView.findViewById(R.id.image);
         // Display the image for the current news in that ImageView
-        if(currentNews.hasImage()) {
-            // Set the ImageView to the image resource specified in the current Word
-            imageView.setImageResource(currentNews.getImageResourceId());
+        if(currentNews.getImage() != "") {
 
+            Bitmap bmp = getBitmapfromUrl(currentNews.getImage());
+
+            imageView.setImageBitmap(bmp);
             //Make sure the view is visible
             imageView.setVisibility(View.VISIBLE);
         }
@@ -92,123 +100,46 @@ public class NewsAdapter extends ArrayAdapter<News> {
         }
 
 
+        // TextView date and time
+        TextView dateView = null;
+        TextView timeView = null;
 
-        // Find the TextView with view ID magnitude
-        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
-        // Format the magnitude to show 1 decimal place
-        String formattedMagnitude = formatMagnitude(currentEarthquake.getMagnitude());
-        // Display the magnitude of the current earthquake in that TextView
-        magnitudeView.setText(formattedMagnitude);
-        // Set the proper background color on the magnitude circle.
-        // Fetch the background from the TextView, which is a GradientDrawable.
-        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
-        // Get the appropriate background color based on the current earthquake magnitude
-        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
-        // Set the color on the magnitude circle
-        magnitudeCircle.setColor(magnitudeColor);
+        if (currentNews.getPublicationDate() != null) {
 
+            // Find the date TextView
+            dateView = listItemView.findViewById(R.id.date);
 
-        // Get the original location string from the Earthquake object,
-        // which can be in the format of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
-        String originalLocation = currentEarthquake.getLocation();
-
-        // If the original location string (i.e. "5km N of Cairo, Egypt") contains
-        // a primary location (Cairo, Egypt) and a location offset (5km N of that city)
-        // then store the primary location separately from the location offset in 2 Strings,
-        // so they can be displayed in 2 TextViews.
-        String primaryLocation;
-        String locationOffset;
-
-        // Check whether the originalLocation string contains the " of " text
-        if (originalLocation.contains(LOCATION_SEPARATOR)) {
-            // Split the string into different parts (as an array of Strings)
-            // based on the " of " text. We expect an array of 2 Strings, where
-            // the first String will be "5km N" and the second String will be "Cairo, Egypt".
-            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
-            // Location offset should be "5km N " + " of " --> "5km N of"
-            locationOffset = parts[0] + LOCATION_SEPARATOR;
-            // Primary location should be "Cairo, Egypt"
-            primaryLocation = parts[1];
-        } else {
-            // Otherwise, there is no " of " text in the originalLocation string.
-            // Hence, set the default location offset to say "Near the".
-            locationOffset = getContext().getString(R.string.near_the);
-            // The primary location will be the full location string "Pacific-Antarctic Ridge".
-            primaryLocation = originalLocation;
         }
 
-        // Find the TextView with view ID location
-        TextView primaryLocationView = (TextView) listItemView.findViewById(R.id.primary_location);
-        // Display the location of the current earthquake in that TextView
-        primaryLocationView.setText(primaryLocation);
 
-        // Find the TextView with view ID location offset
-        TextView locationOffsetView = (TextView) listItemView.findViewById(R.id.location_offset);
-        // Display the location offset of the current earthquake in that TextView
-        locationOffsetView.setText(locationOffset);
+        if (currentNews.getPublicationDate() != null) {
+            dateView = listItemView.findViewById(R.id.date);
+            // Format the date string (i.e. "Mar 3, 1984")
+            String formattedDate = formatDate(currentNews.getPublicationDate()).concat(",");
+            // Display the date of the current earthquake in that TextView
+            dateView.setText(formattedDate);
 
+            // Find the TextView with view ID time
+            timeView = listItemView.findViewById(R.id.time);
+            // Format the time string (i.e. "4:30PM")
+            String formattedTime = formatTime(currentNews.getPublicationDate());
+            // Display the time of the current earthquake in that TextView
+            timeView.setText(formattedTime);
 
-        // Create a new Date object from the time in milliseconds of the earthquake
-        Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
-
-        // Find the TextView with view ID date
-        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
-        // Format the date string (i.e. "Mar 3, 1984")
-        String formattedDate = formatDate(dateObject);
-        // Display the date of the current earthquake in that TextView
-        dateView.setText(formattedDate);
-
-        // Find the TextView with view ID time
-        TextView timeView = (TextView) listItemView.findViewById(R.id.time);
-        // Format the time string (i.e. "4:30PM")
-        String formattedTime = formatTime(dateObject);
-        // Display the time of the current earthquake in that TextView
-        timeView.setText(formattedTime);
+            //Set date & time views as visible
+            dateView.setVisibility(View.VISIBLE);
+            timeView.setVisibility(View.VISIBLE);
+        } else {
+            //Set date & time views as gone
+            dateView.setVisibility(View.GONE);
+            timeView.setVisibility(View.GONE);
+        }
 
         // Return the list item view that is now showing the appropriate data
         return listItemView;
     }
 
-    private int getMagnitudeColor(double magnitude) {
-        int magnitudeColorResourceId;
-        int magnitudeFloor = (int) Math.floor(magnitude);
-        switch (magnitudeFloor) {
-            case 0:
-            case 1:
-                magnitudeColorResourceId = R.color.magnitude1;
-                break;
-            case 2:
-                magnitudeColorResourceId = R.color.magnitude2;
-                break;
-            case 3:
-                magnitudeColorResourceId = R.color.magnitude3;
-                break;
-            case 4:
-                magnitudeColorResourceId = R.color.magnitude4;
-                break;
-            case 5:
-                magnitudeColorResourceId = R.color.magnitude5;
-                break;
-            case 6:
-                magnitudeColorResourceId = R.color.magnitude6;
-                break;
-            case 7:
-                magnitudeColorResourceId = R.color.magnitude7;
-                break;
-            case 8:
-                magnitudeColorResourceId = R.color.magnitude8;
-                break;
-            case 9:
-                magnitudeColorResourceId = R.color.magnitude9;
-                break;
-            default:
-                magnitudeColorResourceId = R.color.magnitude10plus;
-                break;
-        }
-        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
-    }
-
-    /**
+        /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
      */
     private String formatDate(Date dateObject) {
@@ -224,12 +155,28 @@ public class NewsAdapter extends ArrayAdapter<News> {
         return timeFormat.format(dateObject);
     }
 
-    /**
-     * Return the formatted magnitude string showing 1 decimal place (i.e. "3.2")
-     * from a decimal magnitude value.
-     */
-    private String formatMagnitude(double magnitude) {
-        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
-        return magnitudeFormat.format(magnitude);
+
+
+    public Bitmap getBitmapfromUrl(String imageUrl)
+    {
+        try
+        {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
     }
+
+
 }
