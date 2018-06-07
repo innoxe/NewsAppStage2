c6de43a5ea32e638163c5c986c4c6ce3cef3779d
Innoxe <innodima@gmail.com>
2018-06-07 16:50:33 +0200

    Search term Step 3 Override onCreate of NewsPreferenceFragment And cancel default search term

--- app/src/main/java/com/example/android/newsappstage2/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsappstage2/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.newsappstage2;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity{
@@ -12,7 +13,10 @@ public class SettingsActivity extends AppCompatActivity{
     }
 
     public static class NewsPreferenceFragment extends PreferenceFragment {
-
-
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 }
