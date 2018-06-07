d2d371321b924661fef90449eca39a225f2b8b54
Innoxe <innodima@gmail.com>
2018-06-07 13:17:20 +0200

    Making a preference screen. Step3

--- app/src/main/java/com/example/android/newsappstage2/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsappstage2/SettingsActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
     }
